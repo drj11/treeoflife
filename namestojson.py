4e1ccbbf7e398781a2890f6a8e6554582eac464b
David Jones <drj@pobox.com>
2012-08-22 07:30:26 +0100

    creates name.json

---
+++ namestojson.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import itertools
+import json
+import sys
+
+def names(inp, out):
+    r = {}
+    for taxid,group in itertools.groupby(rows(inp), lambda r: r[0]):
+        d = {}
+        for _,name,_,class_ in group:
+            if class_ == 'genbank common name':
+                d['genbank'] = name
+            if class_ == 'common name':
+                d['common'] = name
+            if class_ == 'scientific name':
+                d['scientific'] = name
+        r[taxid] = d
+    json.dump(r, out)
+
+def rows(inp):
+    for line in inp:
+        if line.endswith('\t|\n'):
+            line = line[:-3]
+        l = line.split('\t|\t')
+        yield l
+
+def main():
+    f = open('dump/names.dmp')
+    names(f, sys.stdout)
+
+if __name__ == '__main__':
+    main()
